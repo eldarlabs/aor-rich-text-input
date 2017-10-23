523a19ca4f8db76d34e0d49a81f000b5eb6f0f4f
mogwai <roddhome@catmarks.com>
2017-10-23 14:25:44 +0800

    - update Quill for matchVisual change - remove target for internal links

--- src/index.js
+++ src/index.js
@@ -1,69 +1,84 @@
-import debounce from 'lodash.debounce';
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import Quill from 'quill';
+import debounce from "lodash.debounce";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Quill from "quill";
 
-require('./RichTextInput.css');
+require("./RichTextInput.css");
 
+var Link = Quill.import("formats/link");
+
+class MyLink extends Link {
+  static create(value) {
+    let node = super.create(value);
+    value = this.sanitize(value);
+    node.setAttribute("href", value);
+    if (value.startsWith("https://") || value.startsWith("http://")) {
+        //external link
+    } else {
+        // internal link
+      node.removeAttribute("target");
+    }
+    return node;
+  }
+}
+
+Quill.register(MyLink);
 class RichTextInput extends Component {
-    componentDidMount() {
-        const {
-            input: {
-                value,
-            },
-            toolbar,
-        } = this.props;
+  componentDidMount() {
+    const { input: { value }, toolbar } = this.props;
 
-        this.quill = new Quill(this.divRef, {
-            modules: { toolbar },
-            theme: 'snow',
-        });
+    this.quill = new Quill(this.divRef, {
+      modules: { toolbar },
+      theme: "snow",
+      clipboard: {
+        matchVisual: false,
+      },
+    });
 
-        this.quill.pasteHTML(value);
+    this.quill.pasteHTML(value);
 
-        this.editor = this.divRef.querySelector('.ql-editor');
-        this.quill.on('text-change', debounce(this.onTextChange, 500));
-    }
+    this.editor = this.divRef.querySelector(".ql-editor");
+    this.quill.on("text-change", debounce(this.onTextChange, 500));
+  }
 
-    componentWillUnmount() {
-        this.quill.off('text-change', this.onTextChange);
-        this.quill = null;
-    }
+  componentWillUnmount() {
+    this.quill.off("text-change", this.onTextChange);
+    this.quill = null;
+  }
 
-    onTextChange = () => {
-        this.props.input.onChange(this.editor.innerHTML);
-    }
+  onTextChange = () => {
+    this.props.input.onChange(this.editor.innerHTML);
+  };
 
-    updateDivRef = ref => {
-        this.divRef = ref;
-    }
+  updateDivRef = ref => {
+    this.divRef = ref;
+  };
 
-    render() {
-        return <div className='aor-rich-text-input'>
-            <div ref={this.updateDivRef} />
-        </div>
-    }
+  render() {
+    return (
+      <div className="aor-rich-text-input">
+        <div ref={this.updateDivRef} />
+      </div>
+    );
+  }
 }
 
 RichTextInput.propTypes = {
-    addField: PropTypes.bool.isRequired,
-    addLabel: PropTypes.bool.isRequired,
-    input: PropTypes.object,
-    label: PropTypes.string,
-    options: PropTypes.object,
-    source: PropTypes.string,
-    toolbar: PropTypes.oneOfType([
-        PropTypes.array,
-        PropTypes.bool,
-    ]),
+  addField: PropTypes.bool.isRequired,
+  addLabel: PropTypes.bool.isRequired,
+  input: PropTypes.object,
+  label: PropTypes.string,
+  options: PropTypes.object,
+  source: PropTypes.string,
+  toolbar: PropTypes.oneOfType([PropTypes.array, PropTypes.bool]),
 };
 
 RichTextInput.defaultProps = {
-    addField: true,
-    addLabel: true,
-    options: {},
-    record: {},
-    toolbar: true,
+  addField: true,
+  addLabel: true,
+  options: {},
+  record: {},
+  toolbar: true,
 };
 
 export default RichTextInput;
